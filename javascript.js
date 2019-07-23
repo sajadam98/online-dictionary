4e733d3288235f13182126aef2d0887e5f9d25fb
Sajad Amiri <sajad.vb98@gmail.com>
2019-07-22 22:14:47 -0700

    online-dictionary-with-jquery

--- javascript.js
+++ javascript.js
@@ -1,27 +1,20 @@
-var button = document.getElementById("btn_translate");
-var edtDictionary = document.getElementById("edt_Dictionary");
-var Translated = document.getElementById("txt_translate");
-var selectFrom = document.getElementById("select_from");
-var selectTo = document.getElementById("select_to");
-
-button.addEventListener("click", Translate);
-
-console.log(selectFrom.options[selectFrom.selectedIndex].value);
-var Request = new XMLHttpRequest();
+$("#btn_translate").click(Translate);
 
 function Translate() {
-  button.disabled = true;
-  Request.open(
-    "GET",
-    "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190722T062110Z.5f5465fc8ae5efb9.4218583f102455773fa9f2e8146c8928e25a37f6&lang="
-        + selectFrom.options[selectFrom.selectedIndex].value + "-" + 
-        selectTo.options[selectTo.selectedIndex].value + "&text=" +
-        edtDictionary.value
-  );
-  Request.onload = function() {
-    var data = JSON.parse(Request.responseText);
-    Translated.innerHTML = data.text;
-    button.disabled = false;
-  };
-  Request.send();
+  $("#btn_translate").hide();
+  $.ajax({
+    url:
+      "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190722T062110Z.5f5465fc8ae5efb9.4218583f102455773fa9f2e8146c8928e25a37f6&lang=" +
+      $("#select_from option:selected").val() +
+      "-" +
+      $("#select_to option:selected").val() +
+      "&text=" +
+      $("#edt_Dictionary").val(),
+    success: function(result) {
+      console.log((result.text)[0]);
+      
+      $("#txt_translate").text((result.text)[0]);
+      $("#btn_translate").show();
+    }
+  });
 }
